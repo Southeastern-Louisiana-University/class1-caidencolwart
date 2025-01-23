3f9607cdf4c9153db1dff9343fccba8999a3be98
caidencolwart <caidencolwart@gmail.com>
2025-01-23 12:16:23 -0600

    first commit

---
+++ src/reviewPart1.java
@@ -0,0 +1,15 @@
+package src;
+
+public class reviewPart1{
+    public static void main(String[] args){
+
+        double score;
+        score = 0;
+
+        if(score >= 60){
+            System.out.println("D");
+        } else if (score >= 70) {
+            System.out.println("D");
+        }
+    }
+}
