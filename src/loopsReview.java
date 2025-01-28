b1368f988bc66f63883255c8e943ebb47b4dda23
caidencolwart <caidencolwart@gmail.com>
2025-01-28 12:12:09 -0600

    second commit

--- src/loopsReview.java
+++ src/loopsReview.java
@@ -1,8 +1,27 @@
 package src;
+import java.util.Scanner;
 
+//homework calculate average and display to user. Then provide a letter grade based on ten point scale.
 
 public class loopsReview {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double grade = 0;
+        double sum = 0;
+        double count = 0;
 
+        while (grade >= 0) {
+            System.out.print("Please enter the grade ranging from 0-100 (-1 to terminate): ");
+            grade = input.nextDouble();
+
+            if(grade > 110){
+                System.out.println("Invalid number!");
+            } else{
+                sum += grade;
+                count++;
+            }
+            System.out.println("The count is: " + count + "The sum is: " + sum);
+        }
+        System.out.println("Thanks for using my program");
     }
 }
