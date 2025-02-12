694c54bfd12ce32e490e9b2a75a044244d3d924f
caidencolwart <caidencolwart@gmail.com>
2025-02-12 17:12:11 -0600

    second commit

--- src/Car.java
+++ src/Car.java
@@ -4,15 +4,14 @@ public class Car {
     //Fields(data, state of your object)
     private String make;
     private String model;
-    private final int year;
+    private int year;
     private int mileage;
 
     private String color;
     private String engine;
     private String transmission;
-
     //methods(behavior: how we modify & access data fields)
-    //construct
+    //constructors
     public Car (String make, String model, int year, int mileage, String color, String engine, String transmission) {
         this.make = make;
         this.model = model;
@@ -23,6 +22,7 @@ public class Car {
         this.transmission = transmission;
     }
     //setters
+    //All the attributes should have the ability to be changed because what if the wrong value for that attribute is accidentally set
     public void setMake (String make) {
         this.make = make;
     }
@@ -32,14 +32,17 @@ public class Car {
     public void setColor (String color) {
         this.color = color;
     }
+    public void setYear (int year) {
+        this.year = year;
+    }
     public void setEngine (String engine) {
         this.engine = engine;
     }
     public void setTransmission (String transmission) {
         this.transmission = transmission;
     }
-
     //getters
+    //all the getters are useful to have especially when trying to print information about that object
     public String getMake () {
         return make;
     }
@@ -62,19 +65,30 @@ public class Car {
         return transmission;
     }
 
-    public void drive (int newMiles){
-           if(mileage >= newMiles){
-               mileage = newMiles;
-           } else {
+    //whenever you drive you add miles to your total mileage, your total mileage cant go down
+    public void drive (int miles){
+           if(miles < 0) {
                System.out.println("Can't reverse miles!");
+           } else {
+               this.mileage += miles;
+               System.out.println(miles + " miles added");
            }
     }
+    //prints all known attributes of the car
     public void printCar () {
         System.out.println("The car is a " + getYear() + " " + getMake() + " " + getModel() + "." + " The car is " + getColor() + " and has a " + getEngine() + " engine with a " + getTransmission() + " transmission with " + getMileage() + " miles.");
     }
-
+    //MAIN Method
     public static void main(String[] args) {
         Car car = new Car("Chevy", "Camaro", 2006, 180760, "blue", "v8", "manual");
+        Car car1 = new Car("Honda", "Beat", 2000, 76590, "white", "v6", "automatic");
+        car.printCar();
+        car1.printCar();
+
+        car.drive(100);
+        car.setColor("red");
+        car.setMake("Dodge");
+        car.setModel("Charger");
         car.printCar();
     }
 }
