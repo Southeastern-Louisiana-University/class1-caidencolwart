f68c108c307887471708436d11561ec4a3a12eca
caidencolwart <caidencolwart@gmail.com>
2025-02-15 10:09:03 -0600

    toString experiment second commit

--- src/Car2.java
+++ src/Car2.java
@@ -1,5 +1,6 @@
 package src;
 
+//Provide a description of the car object;similar to a print method. Know the state of an object
 public class Car2 {
     //fields
     public String make;
@@ -11,19 +12,15 @@ public class Car2 {
         this.model = carModel;
         this.year = carYear;
     }
-
-
     public Car2 () {}
     //MANI METHOD
     public static void main(String[] args) {
         Car2 car = new Car2();
         Car2 carEthan = new Car2("Hyundai", "VeraCruz", 2011);
-        carEthan.toString();
-
+        System.out.println("What is carEthan toString: " + carEthan.toString());
     }
-    /*
+    @Override
     public String toString(){
-        return toString() + " new stuff\n";
+        return this.make + " " + this.model + " " + this.year;
     };
-     */
 }
